2913fc1e4a4a886197f3f69bcf7fab0a5e39a840
MeganKR <megankaye2018@gmail.com>
2023-10-17 22:49:33 -0500

    checking if new pull will fix deployment

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,13 @@ var openWeatherKey = "8207e48198911fc2a0c10ea7cc2303da"
 var cityName = ""
 var lat = ""
 var lon = ""
-    // grabs user input data
+// grabs user input data
 var cityInput = $(".city-input")
-    // submissions will be added to this array
+// submissions will be added to this array
 var cityHistory = []
-    // where city list will be generated
+// where city list will be generated
 var cityHistoryList = JSON.parse(localStorage.getItem("city")) || []
-    // where weather data will be generated
+// where weather data will be generated
 var weatherDisplay = $(".weather-display")
 
 
@@ -18,44 +18,44 @@ function getOpenWeather() {
     // variable below is "Geocoding API", says it will make lat & lon from a given city name.
     // var requestUrl= http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=" + 2 + "&appid=" + openWeatherKey;
     fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data){
-        console.log(data)
-        lat = data.coord.lat
-        lon = data.coord.lon
-        getForecast()
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            lat = data.coord.lat
+            lon = data.coord.lon
+            getForecast()
+        })
 
 }
 
 function getForecast() {
     var requestUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + openWeatherKey + "&units=imperial";
     fetch(requestUrl)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function(data){
-      console.log(data)
-      $(".forecast").text("")
-      for (var i = 0; i < data.list.length; i++) {
-        if (data.list[i].dt_txt.includes("12:00:00")) {
-            var card = document.createElement("div")
-            card.setAttribute("class", "card col")
-            var cardBody = document.createElement("div")
-            cardBody.setAttribute("class", "card-body")
-            var date = document.createElement("p")
-            date.textContent = data.list[i].dt_txt.split(" ")[0]
-            var temp = document.createElement("p")
-            temp.textContent = "temp: " + data.list[i].main.temp + "F"
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            $(".forecast").text("")
+            for (var i = 0; i < data.list.length; i++) {
+                if (data.list[i].dt_txt.includes("12:00:00")) {
+                    var card = document.createElement("div")
+                    card.setAttribute("class", "card col")
+                    var cardBody = document.createElement("div")
+                    cardBody.setAttribute("class", "card-body")
+                    var date = document.createElement("p")
+                    date.textContent = data.list[i].dt_txt.split(" ")[0]
+                    var temp = document.createElement("p")
+                    temp.textContent = "temp: " + data.list[i].main.temp + "F"
 
-            cardBody.append(date, temp)
-            card.append(cardBody)
-            $(".forecast").append(card)
-        }
-      }
-    })
+                    cardBody.append(date, temp)
+                    card.append(cardBody)
+                    $(".forecast").append(card)
+                }
+            }
+        })
 }
 // 
 {/* <div class="card col">
